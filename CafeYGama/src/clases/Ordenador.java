c3e94b26e0c3e8eb7fcb13c7d54a100061ab510e
admin <admin@10.10.0.240>
2016-05-23 13:16:39 +0300

    metodo paar genera codigos alatorios

--- CafeYGama/src/clases/Ordenador.java
+++ CafeYGama/src/clases/Ordenador.java
@@ -4,8 +4,23 @@ public class Ordenador {
 	private String Codigo;
 	private int Contador;
 	
-	public Ordenador(String codigo_Ordenador, int contador) {
-		
+	public Ordenador(int contador) {
+		String codigo_Ordenador="";
+			//declaro las variables del rango numérico que corresponde al alfabeto en Ascii
+			//Letras mayúsculas tenemos que usar el rango 65-90
+			int num1 = 65;
+			int num2 = 95;
+			char c = 0;
+			
+			for (int i=1; i<=3; i++){
+				int numAleatorio = (int)Math.floor(Math.random()*(num2 -num1)+num1);
+				c = (char)numAleatorio;
+				codigo_Ordenador = codigo_Ordenador + c;
+			}
+			for (int i=1; i<=3; i++){
+				int numAleatorio = (int)Math.floor(Math.random()*(9 -0)+0);
+				codigo_Ordenador = codigo_Ordenador + numAleatorio;
+			}
 		Codigo = codigo_Ordenador;
 		Contador = contador;
 	}
