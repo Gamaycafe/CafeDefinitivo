b73964b96eb640a1699f85bf4430b81849fe16e8
admin <admin@172.16.194.133>
2016-05-26 13:43:38 +0200

    Cambios conex

--- CafeYGama/src/bbdd/BBDDConexion.java
+++ CafeYGama/src/bbdd/BBDDConexion.java
@@ -16,8 +16,9 @@ public class BBDDConexion {
 	
 	
 	public static void crear(Conexion conexion, Connection c){
-		String cadena="INSERT INTO ordenador VALUES('" + conexion.getNumero_Conexion() 
-				+ "','" + conexion.getCodigo_Ordenador() + "','" + conexion.getCodigo_Socio() 
+		int cont=0;
+		cont ++;
+		String cadena="INSERT INTO ordenador VALUES('" + cont + "','" + conexion.getCodigo_Ordenador() + "','" + conexion.getCodigo_Socio() 
 				+ "','" + conexion.getHora_Inicio() + "','" + conexion.getHora_Final() +"')"; 	
 
 		try{
